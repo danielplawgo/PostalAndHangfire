03350518fe5d39c0c0c783cc50f1162ca0548efd
Daniel Plawgo <daniel@plawgo.pl>
2018-06-18 05:56:52 +0200

    Dodanie przykłądu bazującego na wpisie o bibliotece postal.

---
+++ PostalAndHangfire/PostalAndHangfire/Mailer/UsersMailer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Postal;
+using PostalAndHangfire.Models;
+using PostalAndHangfire.ViewModels.Users;
+
+namespace PostalAndHangfire.Mailer
+{
+    public class UsersMailer : BaseMailer, IUserMailer
+    {
+        public void SendRegisterEmail(User user)
+        {
+            var email = new RegisterEmail()
+            {
+                Email = user.Email,
+                FirstName = user.FirstName
+            };
+
+            //email.Send();
+            Send(email);
+        }
+    }
+
+    public interface IUserMailer
+    {
+        void SendRegisterEmail(User user);
+    }
+}
---
+++ PostalAndHangfire/PostalAndHangfire/Profiles/UsersProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using PostalAndHangfire.Models;
+using PostalAndHangfire.ViewModels.Users;
+
+namespace PostalAndHangfire.Profiles
+{
+    public class UsersProfile : Profile
+    {
+        public UsersProfile()
+        {
+            CreateMap<User, UserViewModel>()
+                .ReverseMap();
+        }
+    }
+}
